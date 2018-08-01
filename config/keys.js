491b629883a359116c32cc71223debd030c73153
Danex2 <khadane.miller@gmail.com>
2018-07-31 23:02:16 -0400

    added login/logout + create profile after registration

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://dane:<password>@ds243049.mlab.com:43049/devconnector",
-  secretOrKey: "<secret>"
+  mongoURI: "mongodb://dane:@ds243049.mlab.com:43049/devconnector",
+  secretOrKey: ""
 };
